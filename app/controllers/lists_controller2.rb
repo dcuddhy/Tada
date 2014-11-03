01e227a9fe11a4b8a145cddc3d7c98e287ec292f
dcuddhy <dcuddhy@gmail.com>
2014-11-02 19:47:06 -0700

    about to add items db

---
+++ app/controllers/lists_controller2.rb
@@ -0,0 +1,39 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    list_params = params.require(:list).permit(:name, :type)
+    @list = List.new
+    @list.save
+    redirect_to lists_path
+  end
+
+    def show
+      @list = List.find(params[:id])
+    end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list_params = params.require(:list).permit(:name, :type)
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to lists_path
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141101221815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: true do |t|
+    t.string  "name"
+    t.integer "type"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.string "name"
+    t.string "type"
+  end
 
 end
